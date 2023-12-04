f077926fa83cd4b2467a9b9277c4b615748efdd9
Maria Sanli <marisanli.93@gmail.com>
2023-12-03 23:28:50 -0500

    CRUD UID updates for user to modify

--- api/authorData.js
+++ api/authorData.js
@@ -3,8 +3,8 @@ import client from '../utils/client';
 const endpoint = client.databaseURL;
 
 // FIXME:  GET ALL AUTHORS
-const getAuthors = () => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/authors.json`, {
+const getAuthors = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors.json?orderBy="uid"&equalTo="${uid}"`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -21,15 +21,18 @@ const getAuthors = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-const getFavoriteAuthor = () => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/authors.json?orderBy="favorite"&equalTo=true`, {
+const getFavoriteAuthor = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors.json?orderBy="uid"&equalTo="${uid}"`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   })
     .then((response) => response.json())
-    .then((data) => resolve(Object.values(data)))
+    .then((data) => {
+      const favAuth = Object.values(data).filter((item) => item.favorite);
+      resolve(favAuth);
+    })
     .catch(reject);
 });
 
